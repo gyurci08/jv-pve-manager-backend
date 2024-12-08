1829071c83198515255aadc711e4346923887f78
György Jandzsó <haver1036@gmail.com>
2024-12-08 21:31:00 +0100

    Init

---
+++ src/test/java/hu/jandzsogyorgy/jvpvemanagerbackend/JvPveManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.jvpvemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvPveManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
